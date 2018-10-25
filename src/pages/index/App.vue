c144d48d7b7c74cacc19243ce56c6949de621d69
trubasa <1141521502@qq.com>
2018-10-25 10:04:55 +0800

    1

---
+++ src/pages/index/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img @click="toDetail" src="./images/logo.png">
+    <hello ></hello>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  import Hello from 'components/Hello/Hello'
+
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    },
+    methods:{
+      toDetail(){
+        window.location.href='../module/detail.html'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
